9bed3a85da572a356f037f4f8cfa969990a4a59f
unknown <Zaebinskiy@yandex.ru>
2020-10-11 05:27:52 +0300

    teper' ya umeyu udalyat'

--- tempp.py
+++
@@ -1,8 +0,0 @@
-""" Вычисляем квадрат по модулю """
-m=int(input('vvedite modul: '))
-for i in range(0, m):
-    j=i*i
-    while j >= m:
-        j -= m
-    if j == 1:
-        print(i,'*', i, '=',j)
