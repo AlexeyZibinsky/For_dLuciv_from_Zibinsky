362cb5b2ee6621d1dd3270e839a72d0fd343c9f6
unknown <Zaebinskiy@yandex.ru>
2021-05-18 00:59:39 +0300

    database homework

---
+++ RDB/11.drop_ddl.sql
@@ -0,0 +1,6 @@
+drop table if exists students;
+drop table if exists programs;
+drop table if exists courses;
+drop table if exists programs_courses;
+drop table if exists marks;
+drop table if exists teachers;
